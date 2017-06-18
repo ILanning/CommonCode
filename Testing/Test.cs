d8571fd754c7c1c97547783d2562db764bbd2125
Ian Lanning <ianclanning@gmail.com>
2017-06-18 12:00:16 -0700

    Added a testing framework

---
+++ Testing/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CommonCode
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void TestAssert()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
